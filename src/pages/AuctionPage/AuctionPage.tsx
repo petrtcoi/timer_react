fb982786a4ba05b792fb15663f22dacf3c2d2a64
Petr Tcoi <petrtcoi@outlook.com>
2022-12-21 02:05:33 +0300

    step 1

--- src/pages/AuctionPage/AuctionPage.tsx
+++ src/pages/AuctionPage/AuctionPage.tsx
@@ -7,44 +7,47 @@ import { useTimerWs } from './hooks/useTimerWs'
 type AuctionPageProps = {}
 
 const AuctionPage: React.FC<AuctionPageProps> = (_props) => {
-  const { auctionId } = useParams()
 
-  const timerOne = useTimer(auctionId || 'test')
+return (<>Page</>)
+  
+//   const { auctionId } = useParams()
 
-  const [userName, setUserName] = React.useState<string>('')
-  const handleSubmit = (event: any) => {
-    event.preventDefault()
-    setUserName(event.target[0].value)
-  }
-  const seconds = useTimerWs(auctionId || 'test', userName)
+//   const timerOne = useTimer(auctionId || 'test')
 
- if (!timerOne || !auctionId) return <>...</>
+//   const [userName, setUserName] = React.useState<string>('')
+//   const handleSubmit = (event: any) => {
+//     event.preventDefault()
+//     setUserName(event.target[0].value)
+//   }
+//   const seconds = useTimerWs(auctionId || 'test', userName)
 
-  return (
-    <div style={ { padding: "20px" } }>
-      <h2>Аукцион: { auctionId }</h2>
-      <div style={ { marginTop: "50px" } }>
-        <DisplayTime
-          { ...timerOne }
-        />
-        <p>Таймер 1: запускает таймер с указанным id при перезагрузке страницы. Далее делает GET запросы на сервер для получения текущего состояния счетчика. На сервере данные отдаются не сразу: сервер дожидается обновления секунды и после этого дает ответ. Таким образом,запросы идут раз в секунду.</p>
-      </div>
-      <div style={ { marginTop: "50px" } }>
-        <DisplayTime
-          loopDuration={ timerOne.loopDuration }
-          secondsPassed={ seconds }
-        />
-        <form onSubmit={ handleSubmit }>
-          <label>
-            Имя пользователя:
-            <input type="text" name="name" />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-        <p>Таймер 2: Подключается к уже готовому счетчику через WebSocket и получает данные с сервера по этому каналу</p>
-      </div>
-    </div>
-  )
+//  if (!timerOne || !auctionId) return <>...</>
+
+//   return (
+//     <div style={ { padding: "20px" } }>
+//       <h2>Аукцион: { auctionId }</h2>
+//       <div style={ { marginTop: "50px" } }>
+//         <DisplayTime
+//           { ...timerOne }
+//         />
+//         <p>Таймер 1: запускает таймер с указанным id при перезагрузке страницы. Далее делает GET запросы на сервер для получения текущего состояния счетчика. На сервере данные отдаются не сразу: сервер дожидается обновления секунды и после этого дает ответ. Таким образом,запросы идут раз в секунду.</p>
+//       </div>
+//       <div style={ { marginTop: "50px" } }>
+//         <DisplayTime
+//           loopDuration={ timerOne.loopDuration }
+//           secondsPassed={ seconds }
+//         />
+//         <form onSubmit={ handleSubmit }>
+//           <label>
+//             Имя пользователя:
+//             <input type="text" name="name" />
+//           </label>
+//           <input type="submit" value="Submit" />
+//         </form>
+//         <p>Таймер 2: Подключается к уже готовому счетчику через WebSocket и получает данные с сервера по этому каналу</p>
+//       </div>
+//     </div>
+//   )
 }
 
 export default AuctionPage
