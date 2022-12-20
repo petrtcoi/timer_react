2a5710ccf60c5554cd4da07ede27464e20615909
Petr Tcoi <petrtcoi@outlook.com>
2022-12-20 13:56:24 +0300

    netlify

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -5,10 +5,10 @@ type HomePageProps = {}
 const HomePage: React.FC<HomePageProps> = (_props) => {
   return(
     <div>
-      <div>Страница аукциона: "/auction/:auctionID"</div>
+      <div>Страница аукциона: "/auctions/:auctionID"</div>
       <div><a href="/_inspect" target="_blank">Инспекция всех аукционов</a></div>
       <div style={{marginTop: "100px"}}>
-        Удалить акцион:DELETE запрос "http://45.12.18.61/auction/:auctionID"
+        Удалить акцион: DELETE запрос "https://45.12.18.61/auction/:auctionID"
       </div>
     </div>
     
