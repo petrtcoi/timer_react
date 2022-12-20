1d2c61dad940f333e429d3aa1d541075c583d880
Petr Tcoi <petrtcoi@outlook.com>
2022-12-21 02:06:37 +0300

    step 2

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={ <HomePage /> } />
         <Route path="/_inspect" element={ <InspectPage /> } />
-        <Route path="/auctions/:auctionId" element={ <AuctionPage /> } />
+        <Route path="/auctions/auctionId" element={ <AuctionPage /> } />
         <Route path="/*" element={ <ErrorPage /> } />
       </Routes>
     </div>
